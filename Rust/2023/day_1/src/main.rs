f03018c8c959aa0d5944d954ac74590c612e6623
ThePixelCode <thepixelcode@proton.me>
2023-12-04 21:35:36 +0000

    First try if solution

--- Rust/2023/day_1/src/main.rs
+++ Rust/2023/day_1/src/main.rs
@@ -2,6 +2,15 @@ use std::env::args;
 use std::fs::OpenOptions;
 use std::io::Read;
 
+fn found_digits(input: &str) -> u32 {
+    let mut digits = input.chars().filter_map(|x| x.to_digit(10));
+    let first = digits.nth(0).unwrap();
+    if let Some(last) = digits.last() {
+        return (first * 10) + last;
+    }
+    (first * 10) + first
+}
+
 fn main() {
     let args: Vec<String> = args().collect();
     let file_name = &args[1];
@@ -9,4 +18,11 @@ fn main() {
     let mut input = String::new();
     file.read_to_string(&mut input).unwrap();
     println!("{}", &input);
+    let mut sum = 0;
+    for line in input.lines() {
+        let found = found_digits(line);
+        println!("found: {}", &found);
+        sum += found;
+    }
+    println!("Sum: {}", sum)
 }
