64a02b5e955cd9135f428d5b53c1284441bc570a
ThePixelCode <thepixelcode@proton.me>
2023-12-24 17:30:35 +0000

    Problem 1 solved

--- Rust/2023/day_6/src/main.rs
+++ Rust/2023/day_6/src/main.rs
@@ -1,4 +1,4 @@
-use std::{env::args, fs::OpenOptions, io::Read, process::exit};
+use std::{env::args, fs::OpenOptions, io::Read, process::exit, rc::Rc};
 
 fn main() {
     let args = args().collect::<Vec<String>>();
@@ -21,8 +21,68 @@ fn main() {
     }
 }
 
+fn parse_input(input: &str) -> Rc<[(u32, u32)]> {
+    let mut times = None;
+    let mut distances = None;
+    for line in input.lines() {
+        if line.is_empty() {
+            continue;
+        }
+        if line.starts_with("Time:") {
+            times = Some(
+                line.split_whitespace()
+                    .skip(1)
+                    .filter_map(|x| {
+                        if x.is_empty() {
+                            return None;
+                        }
+                        if let Ok(n) = x.parse::<u32>() {
+                            return Some(n);
+                        }
+                        unreachable!()
+                    })
+                    .collect::<Vec<u32>>(),
+            );
+            continue;
+        }
+        if line.starts_with("Distance:") {
+            distances = Some(
+                line.split_whitespace()
+                    .skip(1)
+                    .filter_map(|x| {
+                        if x.is_empty() {
+                            return None;
+                        }
+                        if let Ok(n) = x.parse::<u32>() {
+                            return Some(n);
+                        }
+                        unreachable!()
+                    })
+                    .collect::<Vec<u32>>(),
+            );
+            continue;
+        }
+        unreachable!()
+    }
+    let times = times.unwrap();
+    let distances = distances.unwrap();
+    times
+        .iter()
+        .zip(distances.iter())
+        .map(|(x, y)| (*x, *y))
+        .collect::<Rc<[(u32, u32)]>>()
+}
+
+fn search_combinations(time_and_distance: &(u32, u32)) -> usize {
+    (0..time_and_distance.0)
+        .filter(|x| (x * (time_and_distance.0 - x)) > time_and_distance.1)
+        .count()
+}
+
 fn solve_part_1(file_content: String) {
-    todo!()
+    let data = parse_input(&file_content);
+    let product = data.iter().map(search_combinations).product::<usize>();
+    println!("Products: {}", product);
 }
 
 fn solve_part_2(file_content: String) {
