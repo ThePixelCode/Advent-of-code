7e9e7cfb68db7ba8b862310a5fb925d392f8f74f
ThePixelCode <thepixelcode@proton.me>
2024-02-20 16:28:31 -0500

    Problem 9 initial commit

---
+++ Rust/2023/day_9/src/lib.rs
@@ -0,0 +1,28 @@
+#[derive(Debug, clap::Parser)]
+#[command(version, about="Solve advent of code", long_about=None)]
+pub struct Args {
+    #[arg(short, long)]
+    file: String,
+    #[arg(short, long, value_parser(1..=2))]
+    method: u8,
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum CommonErrors {
+    #[error("IOError: {0}")]
+    IOError(#[from] std::io::Error),
+}
+
+type CommonError<T> = Result<T, CommonErrors>;
+
+impl Args {
+    pub fn get_info(self) -> CommonError<(std::fs::File, u8)> {
+        let file = std::fs::OpenOptions::new()
+            .read(true)
+            .write(false)
+            .create_new(false)
+            .open(self.file)?;
+        Ok((file, self.method))
+    }
+}
+
