5ee63b42b1a17251f7e9cb6785161e2ec014d556
ThePixelCode <thepixelcode@proton.me>
2024-02-20 16:45:06 -0500

    Problem 15 initial commit

---
+++ Rust/2023/day_15/src/main.rs
@@ -0,0 +1,44 @@
+use std::fmt::{Debug, Display};
+
+fn main() {
+    use clap::Parser;
+    let (mut file, method) = match advent_of_code_day_15::Args::parse().get_info() {
+        Ok(args) => args,
+        Err(e) => {
+            eprintln!("{:?}", e);
+            std::process::exit(1)
+        }
+    };
+
+    let mut buffer = String::new();
+    match std::io::Read::read_to_string(&mut file, &mut buffer) {
+        Ok(_) => (),
+        Err(e) => {
+            eprintln!("{:?}", e);
+            std::process::exit(1)
+        }
+    };
+
+    let result = match method {
+        1 => solve_method_1::<u64>(buffer),
+        2 => solve_method_2::<u64>(buffer),
+        _ => unreachable!(),
+    };
+    println!("{}", result)
+}
+
+#[allow(dead_code)]
+fn solve_method_1<T>(#[allow(unused_variables)] text: String) -> T
+where
+    T: From<u64> + Debug + Display,
+{
+    todo!()
+}
+
+#[allow(dead_code)]
+fn solve_method_2<T>(#[allow(unused_variables)] text: String) -> T
+where
+    T: From<u64> + Debug + Display,
+{
+    todo!()
+}
