d3a8de86471b5827f5a04d8641dbc68b46f77318
ThePixelCode <thepixelcode@proton.me>
2024-12-08 21:57:55 -0500

    2024/day_01: Solved part 1 and 2

--- 2024/day_01/src/lib.rs
+++ 2024/day_01/src/lib.rs
@@ -6,3 +6,33 @@ pub struct Arg {
     #[arg(short, long, value_name = "INPUT_FILE")]
     pub file: std::path::PathBuf,
 }
+
+pub fn from_str(line: &str) -> Result<(u32, u32), ()> {
+    let (left, right) = line.split_once("   ").ok_or(())?;
+    Ok((left.parse().or(Err(()))?, right.parse().or(Err(()))?))
+}
+
+pub fn compress<T>(vecs: (Vec<T>, Vec<T>)) -> Result<Vec<(T, T)>, ()> {
+    if vecs.0.len() != vecs.1.len() {
+        return Err(());
+    }
+
+    Ok(vecs.0.into_iter().zip(vecs.1.into_iter()).collect())
+}
+
+pub fn decompress<T>(vecs: Vec<(T, T)>) -> (Vec<T>, Vec<T>) {
+    vecs.into_iter().collect()
+}
+
+pub fn get_file_contents<P: AsRef<std::path::Path>>(path: P) -> std::io::Result<String> {
+    use std::io::Read;
+    let mut file = std::fs::OpenOptions::new()
+        .read(true)
+        .write(false)
+        .open(path)?;
+    let mut content = String::new();
+
+    file.read_to_string(&mut content)?;
+
+    Ok(content)
+}
--- 2024/day_01/src/main.rs
+++ 2024/day_01/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
+    use clap::Parser;
+    let arg = aoc_day_01::Arg::parse();
 
+    let file = aoc_day_01::get_file_contents(arg.file).unwrap();
+
+    let pairs = file.lines().map(|line|aoc_day_01::from_str(line).unwrap()).collect::<Vec<(u32,u32)>>();
+
+    let result = if arg.part == 1 {
+        let mut pairs = aoc_day_01::decompress(pairs);
+
+        pairs.0.sort();
+        pairs.1.sort();
+
+        aoc_day_01::compress(pairs).unwrap().into_iter().map(|(a, b)| a.abs_diff(b)).sum::<u32>()
+    } else {
+        let pairs = aoc_day_01::decompress(pairs);
+
+        pairs.0.into_iter().map(|l| l * (pairs.1.iter().filter(|r| l == **r).count() as u32)).sum::<u32>()
+    };
+
+    println!("RESULT: {}", result);
 }
