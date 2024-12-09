ee6ab26eae9358b26d41a2bb7d3ac946282951b1
ThePixelCode <thepixelcode@proton.me>
2024-12-08 23:45:12 -0500

    2024/day_02: Part 1 solved

--- 2024/day_02/src/lib.rs
+++ 2024/day_02/src/lib.rs
@@ -20,3 +20,33 @@ pub fn get_file_contents<P: AsRef<std::path::Path>>(path: P) -> std::io::Result<
     Ok(content)
 }
 
+pub fn get_report(line: &str) -> Result<Vec<u32>, ()> {
+    line.split(" ").map(|l| l.parse::<u32>().or(Err(()))).collect::<Result<Vec<u32>, ()>>()
+}
+
+const TOLERANCE: std::ops::RangeInclusive<u32> = 1..=3;
+
+pub fn is_safe(report: &Vec<u32>) -> bool {
+    let mut last_level = None;
+    let mut increasing = None;
+
+    for level in report {
+        if let Some(last_level) = last_level {
+            if !TOLERANCE.contains(&level.abs_diff(last_level)) {
+                return false;
+            }
+            let is_increasing = *level > last_level;
+
+            if let Some(increasing) = increasing {
+                if increasing != is_increasing {
+                    return false;
+                }
+            } else {
+                increasing = Some(is_increasing);
+            }
+        }
+        last_level = Some(*level);
+    }
+
+    true
+}
--- 2024/day_02/src/main.rs
+++ 2024/day_02/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
+    use clap::Parser;
+    let arg = aoc_day_02::Arg::parse();
 
+    let file = aoc_day_02::get_file_contents(arg.file).unwrap();
+
+    let reports = file.lines().map(|line| aoc_day_02::get_report(line)).collect::<Result<Vec<Vec<u32>>, ()>>().unwrap();
+
+    let result = if arg.part == 1 {
+        reports.into_iter().filter(|report| aoc_day_02::is_safe(report)).count() as u32
+    } else {
+        todo!()
+    };
+
+    println!("RESULT: {}", result);
 }
